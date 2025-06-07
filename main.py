a7151ae687151e73358f371fe192e12b3abd2439
Jose Mejia <enriquemejiagamarra@gmail.com>
2025-06-07 11:11:12 +0000

    test:use alias IP

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 def main():
     model = {"title": "Hello Build Trigger."}
     return render_template("index.html", model=model)
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=8080, debug=True)
